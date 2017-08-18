a10e5918a4c1e07e1e91926bf7f78aabf7aca7c7
Mathieu Revillion <mathieu.revillion@gmail.com>
2017-08-18 11:09:46 +0200

    "worked on the homepage (real SPA)"

--- client/src/homepage/Homepage.vue
+++ client/src/homepage/Homepage.vue
@@ -1,38 +1,90 @@
 <template>
   <div class="homepage">
-    <br>
-    <h1>{{ msg }}</h1>
-    <br>
-    <!-- <h3>Discover what are roleplaying games</h3>
-    <h3>Reach other players and start having fun</h3> -->
-    <h3>Discover what Roleplaying Game is</h3>
-    <br>
-    <h3>Join a session near you</h3>
-    <br>
-    <h3>Create a new character for your next session</h3>
-    <br>
-    <br>
-    <br>
+    <div class="tile is-parent is-12">
 
-    <div class="tile is-parent">
-      <article class="tile is-child notification is-primary">
-        <p class="title">Vertical...</p>
-        <p class="subtitle">Top tile</p>
-      </article>
+      <div class="tile is-child is-4 box">
+        <article class="tile notification is-primary">
+          <div class="content">
+            <p class="title">Welcome to RPGLand !</p>
+            <br>
+            <h3 class="subtitle">Find RPG Clubs near you</h3>
+            <br>
+            <h3 class="subtitle">Discover </h3>
+            <br>
+            <h3 class="subtitle">Create a new character for your next session (Soon ...)</h3>
+          </div>
+        </article>
+      </div>
+      <br>
+
+      <div class="tile is-child is-4 box">
+        <article class="tile notification is-primary">
+          <div class="content">
+            <p class="title">Find a Club near you !</p>
+            <br>
+            <h3 class="subtitle">Look for the club that suits you</h3>
+            <br>
+            <h3 class="subtitle">See its location on the map</h3>
+            <br>
+            <h3 class="subtitle">Discuss with its Game Master and other members</h3>
+          </div>
+        </article>
+      </div>
+      <br>
+
+      <div class="tile is-child is-4 box">
+        <article class="tile notification is-primary">
+          <div class="content">
+            <p class="title">Join the Community !</p>
+            <br>
+            <h3 class="subtitle">Join Game Masters (GM) and other players</h3>
+            <br>
+            <h3 class="subtitle">Live adventures together</h3>
+            <br>
+            <h3 class="subtitle">Let your shared imagination drive you far far away</h3>
+          </div>
+        </article>
+      </div>
+      <br>
+
+      <!-- <div class="tile is-child is-4 box">
+        <div class="content">
+          <article class="tile notification is-primary">
+            <p class="title">Find a Club near you !</p>
+            <br>
+            <h3 class="subtitle">Discover what Roleplaying Game is</h3>
+            <br>
+            <h3 class="subtitle">Join a session near you</h3>
+            <br>
+            <h3 class="subtitle">Create a new character for your next session</h3>
+          </article>
+        </div>
+      </div>
+      <br>
+
+      <div class="tile is-child is-4 box">
+        <div class="content">
+          <article class="tile notification is-primary">
+            <p class="title">Join the Community !</p>
+            <br>
+            <h3 class="subtitle">Discover what Roleplaying Game is</h3>
+            <br>
+            <h3 class="subtitle">Join a session near you</h3>
+            <br>
+            <h3 class="subtitle">Create a new character for your next session</h3>
+          </article>
+        </div>
+      </div> -->
     </div>
-</div>
-
-
   </div>
+
 </template>
 
 <script>
 export default {
   name: "hello",
   data() {
-    return {
-      msg: "Welcome to JDRLand"
-    };
+    return {};
   }
 };
 </script>
@@ -41,7 +93,7 @@ export default {
 <style scoped>
 
 .homepage {
-  background-color: grey;
+  background-color: white;
 }
 
 h1, h3 {
@@ -62,6 +114,10 @@ li {
 .link {
   color: white;
   text-decoration: italic
+}
+
+.box{
+  height: 200px;
 }
 
 
--- client/src/whatisrpg/Whatisrpg.vue
+++ client/src/whatisrpg/Whatisrpg.vue
@@ -1,22 +1,25 @@
 <template lang="html">
   <div class="tile is-parent">
-   <article class="what tile is-child">
+   <article id="what" class="tile is-child">
      <p class="title">What is Table RPG ?</p>
      <p class="subtitle">Top tile</p>
    </article>
   </div>
 
   <div class="tile is-parent">
-   <article class="why tile is-child">
-     <p class="title">Vertical...</p>
-     <p class="subtitle">Top tile</p>
+   <article id="why" class="tile is-child">
+     <p class="title">Why is it awesome ?</p>
+     <p class="subtitle">You share adventures with your friends and acquaintances</p>
+     <p class="subtitle">Discover the true power of your imagination</p>
+     <p class="subtitle">Learn the basics of acting and improvisation through games</p>
+     <p class="subtitle">Bond with new people through exciting </p>
    </article>
   </div>
 
   <div class="tile is-parent">
-   <article class="how tile is-child">
-     <p class="title">Vertical...</p>
-     <p class="subtitle">Top tile</p>
+   <article  id="how" class="tile is-child">
+     <p class="title">How to Play ?</p>
+     <p class="subtitle">The main </p>
    </article>
   </div>
 </template>
